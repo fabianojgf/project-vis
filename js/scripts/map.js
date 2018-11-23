d5f49bedc82783d1cf7c319a6738723c6ca45404
arthursb <arthursbastos1@hotmail.com>
2018-11-23 19:13:11 -0300

    mapa colorido

--- js/scripts/map.js
+++ js/scripts/map.js
@@ -1,9 +1,5 @@
 let width = 960, height = 600;
 
-let quantize = d3.scaleQuantize()
-    .range(d3.schemeGreens[9])
-    .domain([2,10]);
-
 let svg = d3.select("body").append("svg")
     .attr("width", width)
     .attr("height", height);
@@ -12,6 +8,7 @@ let promises = [d3.json("data/br-states.json")];
 Promise.all(promises).then(ready);
 
 function ready([br]) {
+
     var states = topojson.feature(br, br.objects.states);
 
     var projection = d3.geoIdentity()
@@ -20,12 +17,36 @@ function ready([br]) {
 
     let path = d3.geoPath().projection(projection);
 
+    console.log(states);
+
     svg.append("g")
       .attr("class", "states")
     .selectAll("path")
       .data(states.features)
     .enter().append("path")
-      .attr("fill", function(d) { return quantize(5); })
+      .attr("fill", function() { console.log(1234);})
       .attr("d", path);
+}
+
+function getColor(stateName){
+  var scheme = d3.schemeCategory10;
+
+  console.log(d);
+
+  switch(stateName){
+    case 'CENTRO-OESTE':
+      return scheme[1];
+    case 'SUL':
+      return scheme[2];
+    case 'SUDESTE':
+      return scheme[3];
+    case 'NORTE':
+      return scheme[4];
+    case 'NORDESTE':
+      return scheme[5];
+    default:
+      return scheme[0];
+
+  }
 }
 
