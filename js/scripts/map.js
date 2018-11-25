eb94ae996be4f6c7f8409c9ecee66aba73ecc932
arthursb <arthursbastos1@hotmail.com>
2018-11-25 10:15:34 -0300

    mapa colorido por região + com highlight no mouseover

--- js/scripts/map.js
+++ js/scripts/map.js
@@ -17,35 +17,47 @@ function ready([br]) {
 
     let path = d3.geoPath().projection(projection);
 
-    console.log(states);
-
     svg.append("g")
       .attr("class", "states")
     .selectAll("path")
       .data(states.features)
     .enter().append("path")
-      .attr("fill", function() { console.log(1234);})
-      .attr("d", path);
+      .attr("fill", function(d) { return getColor(d.properties.region);})
+      .attr("d", path)
+    .on("mouseover", function(d){
+        d3.select(this)
+        .style("cursor", "pointer")
+        .attr("stroke-width", 5)
+        .attr("stroke","#FFF5B1");
+      })
+      .on("mouseout", function(d){
+        d3.select(this)
+        .style("cursor", "default")
+        .attr("stroke-width", 1)
+        .attr("stroke","#eee");
+      });
 }
+
+
 
 function getColor(stateName){
   var scheme = d3.schemeCategory10;
 
-  console.log(d);
+  console.log(stateName);
 
   switch(stateName){
     case 'CENTRO-OESTE':
       return scheme[1];
     case 'SUL':
-      return scheme[2];
+      return scheme[9];
     case 'SUDESTE':
-      return scheme[3];
-    case 'NORTE':
-      return scheme[4];
-    case 'NORDESTE':
-      return scheme[5];
-    default:
       return scheme[0];
+    case 'NORTE':
+      return scheme[2];
+    case 'NORDESTE':
+      return scheme[3];
+    default:
+      return scheme[7];
 
   }
 }
