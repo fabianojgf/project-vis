ce684d310c83ff20701d3ead3b78d2125224e071
arthursb <arthursbastos1@hotmail.com>
2018-12-01 15:18:21 -0300

    renderizando gráfico comparativo entre o salario do deputado e outras coisas falta ajeitar as labels e cores

--- js/scripts/comps.js
+++ js/scripts/comps.js
@@ -1,3 +1,45 @@
 const desenharComps = (seletor, comparacoes) => {
-  console.log(comparacoes);
+
+    let facts = crossfilter(comparacoes);
+    let nomeDim = facts.dimension(d => {return d.nome});
+    let nomeGroup = nomeDim.group().reduceSum(d => {return d.valor});
+    console.log(nomeGroup.all());
+
+    var valoresExtent = d3.extent(nomeGroup.all(), d => {return d.value});
+    var valoresMin = valoresExtent[0];
+    var valoresMax = valoresExtent[1];
+
+    console.log(nomeGroup.all().sort(function(a, b) {return d3.ascending(a.value, b.value);}));
+    console.log(valoresMin + " " + valoresMax);
+
+    let barchart = dc.barChart(seletor);
+    barchart.ordering(function (d) { return -d.value ;})
+            .width(600)
+            .height(600)
+            .margins({top: 50, right: 50, bottom: 25, left: 60})
+            .y(d3.scaleLinear().domain([0,valoresMax]))
+            .x(d3.scaleBand())
+            .xUnits(dc.units.ordinal)
+            .brushOn(false)
+            .outerPadding(1)
+            .dimension(nomeDim)
+            .group(nomeGroup)
+            .colors(d3.scaleOrdinal().domain(["isLessThanSalario", "isSalario", "isBiggerThanSalario"]).range(["#00FF00", "#0000FF", "#FF0000"]))
+            .colorAccessor(function(d) {
+              console.log(d.value);
+              if (d.value === 33763) {
+                return "isSalario";
+              }
+              
+              if(d > 33764){
+                console.log("maior" + d);
+                return "isBiggerThanSalario";
+              }
+              else{
+                console.log("menor" + d);
+                return "isLessThanSalario";
+              }
+            });
+    barchart.render();
+    
 };
--- js/scripts/main.js
+++ js/scripts/main.js
@@ -37,7 +37,7 @@ $(document).ready(() => {
         })
 
         carregarComparacao().then(comps => {
-           desenharComps("#grafico-comparacoes" ,comps);
+           desenharComps("#grafico-comparacoes", comps);
         }).catch(err => {
             console.log(err);
         })
